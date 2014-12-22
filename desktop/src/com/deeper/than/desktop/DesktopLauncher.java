dc3a6a49c58c85fb9809f7485401c1c09d6312ad
Zylox <zch5of6@earthlink.net>
2014-12-22 02:14:29 -0600

    first commit

---
+++ desktop/src/com/deeper/than/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.deeper.than.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.deeper.than.DeeperThanLight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DeeperThanLight(), config);
+	}
+}
---
+++ ios/src/com/deeper/than/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.deeper.than;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.deeper.than.DeeperThanLight;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DeeperThanLight(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
